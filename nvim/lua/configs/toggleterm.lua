bcfa0adba4f5b7f03a25460136dae088c1c6a4ec
Dylan Parker <dylanparkerr@gmail.com>
2022-06-27 23:59:26 -0400

    added lazygit toggleterm

--- nvim/lua/configs/toggleterm.lua
+++ nvim/lua/configs/toggleterm.lua
@@ -14,3 +14,30 @@ require('toggleterm').setup{
     shell = 'bash' -- change the default shell
     -- shell = 'git-bash' -- change the default shell
 }
+
+-- custom floating term for lazygit
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  close_on_exit = true, -- this doesn't work for some reason :(
+  direction = "float",
+  float_opts = {
+    border = "curved",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("Closing terminal")
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})

2894b65acd3e089fd4e9f3e15db1d906510032fa
dylan parker <dylanparkerr@gmail.com>
2024-09-22 20:28:07 -0400

    fix typo - add other catppuccin themes

--- nvim/lua/configs/catppuccin.lua
+++ nvim/lua/configs/catppuccin.lua
@@ -1,3 +1,4 @@
 -- vim.cmd.colorscheme("catppuccin")
-vim.cmd.colorscheme("catppuccin-frappe")
--- vim.cmd.colorscheme("catppuccin-mocchiato")
+-- vim.cmd.colorscheme("catppuccin-frappe")
+vim.cmd.colorscheme("catppuccin-macchiato")
+-- vim.cmd.colorscheme("catppuccin-mocha")

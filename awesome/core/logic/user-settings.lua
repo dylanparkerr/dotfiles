d7540dcb4c7335ae04e0216bc92cf67b3d856e74
dylan parker <dylanparkerr@gmail.com>
2024-09-26 21:52:32 -0400

    cleanup

--- awesome/core/logic/user-settings.lua
+++
@@ -1,7 +0,0 @@
-local settings = {
-    terminal = "alacritty",
-    editor = "nvim",
-    browser = "brave",
-    modkey = "Mod4"
-}
-return settings
--- scripts/connect-sony.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pavucontrol&
-sleep 0.5
-killall pavucontrol
-
-bluetoothctl power on
-pulseaudio --start
-bluetoothctl connect 94:DB:56:F7:F3:6E
-

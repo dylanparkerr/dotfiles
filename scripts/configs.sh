45d5a01ab3a82f4f8b2084ab60c46cb74e3b39f6
Dylan Parker <dylanparkerr@gmail.com>
2026-04-19 21:42:39 -0400

    add missing ghostty link

--- scripts/configs.sh
+++ scripts/configs.sh
@@ -4,6 +4,7 @@ dirs=(
   "$HOME/.config/tmux/"
   "$HOME/.config/tmux/plugins/"
   "$HOME/.config/lazygit/"
+  "$HOME/.config/ghostty/"
   "$HOME/.config/mise/"
   "$HOME/.config/mise/conf.d/" # for conditional global configs 
 )
@@ -17,6 +18,7 @@ ln -sf $HOME/repos/dotfiles/bash/.bashrc $HOME/.bashrc
 ln -sf $HOME/repos/dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -sf $HOME/repos/dotfiles/nvim $HOME/.config/nvim
 ln -sf $HOME/repos/dotfiles/lazygit/config.yml $HOME/.config/lazygit/config.yml
+ln -sf $HOME/repos/dotfiles/ghostty/config $HOME/.config/ghostty/config
 ln -sf $HOME/repos/dotfiles/starship/starship.toml $HOME/.config/starship.toml
 ln -sf $HOME/repos/dotfiles/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
 ln -sf $HOME/repos/dotfiles/mise/config.toml $HOME/.config/mise/config.toml
